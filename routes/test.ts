295d946c8cae4a33c9b1c4c08f7c0d0c82a0fae6
lw <1778768609@qq.com>
2019-05-06 18:46:59 +0800

    :sparkles: add typeScript

---
+++ routes/test.ts
@@ -0,0 +1,15 @@
+const router = require('koa-router')()
+
+router.prefix('/test')
+
+router.get('/', function (ctx: any, next: any) {
+  debugger
+
+  ctx.body = 'this is a test234 response!'
+})
+
+router.get('/bar', function (ctx: any, next: any) {
+  ctx.body = 'this is a users/bar response'
+})
+
+module.exports = router
--- src/db/mysql.js
+++ src/db/mysql.js
@@ -9,17 +9,27 @@ const pool = mysql.createPool({
 })
 
 exports.query = function (sql, arr, callback) {
+  if (!sql || !arr) return
   // 建立链接
   pool.getConnection(function (err, connection) {
-    if (err) {
-      throw err
+    if (err) throw err
+    if (callback) {
+      // 三个参数时
+      connection.query(sql, arr, function (error, results, fields) {
+        connection.release()
+        if (error) throw error
+        // 执行回调函数，将数据返回
+        callback(results, fields)
+      })
+    } else {
+      // 两个参数时
+      let fn = arr
+      connection.query(sql, function (error, results, fields) {
+        connection.release()
+        if (error) throw error
+        // 执行回调函数，将数据返回
+        fn && fn(results, fields)
+      })
     }
-    connection.query(sql, arr, function (error, results, fields) {
-      // 将链接返回到连接池中，准备由其他人重复使用
-      connection.release()
-      if (error) throw error
-      // 执行回调函数，将数据返回
-      callback && callback(results, fields)
-    })
   })
 }
